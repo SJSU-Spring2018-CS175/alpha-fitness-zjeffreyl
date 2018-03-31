d7bbf567a44364d5736cfbfe6689a5e838e5feb2
zjeffreyl <zjeffreyl@gmail.com>
2018-03-31 16:37:45 -0700

    Implement states and timer

--- app/src/main/java/com/example/zjeff/alphafitness/MainActivity.java
+++ app/src/main/java/com/example/zjeff/alphafitness/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import com.google.android.gms.common.GoogleApiAvailability;
 
--- app/src/main/java/com/example/zjeff/alphafitness/MapsActivity.java
+++ app/src/main/java/com/example/zjeff/alphafitness/MapsActivity.java
@@ -4,12 +4,17 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.nfc.Tag;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -23,30 +28,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
-
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        Toast.makeText(this, "Map is Ready HERE", Toast.LENGTH_SHORT).show();
-        mMap = googleMap;
-        Log.d(TAG, "" + mLocationPermissionGranted);
-        if (mLocationPermissionGranted) {
-            getDeviceLocation();
-            //mark location
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                    && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
-                return;
-            }
-            mMap.setMyLocationEnabled(true);
-        }
-    }
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback{
 
     private GoogleMap mMap;
     private static final String TAG = "MapActivity";
@@ -56,13 +38,75 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
     private FusedLocationProviderClient mFusedLocationProviderClient;
     private static final float DEFAULT_ZOOM = 15f;
+    Button recordButton;
+    //Stopwatch
+    TextView duration;
+    int MilliSeconds, Seconds, Minutes;
+    long MillisecondTime, StartTime, UpdateTime, TimeBuff = 0L;
+    Handler handler;
+    //record states
+    public enum recordState{START, STOP, REST};
+    public recordState state = recordState.REST;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         getLocationPermission();
+        //Stopwatch
+        recordButton = (Button)findViewById(R.id.recordButton);
+        recordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Start goes to stop
+                if(state == recordState.START){
+                    //Everything that happens at stop here
+                    TimeBuff += MillisecondTime;
+                    handler.removeCallbacks(runnable);
+                    //Option to Reset
+                    recordButton.setText("Reset");
+                    state = recordState.STOP;
+                }else
+                //Stop goes to rest
+                if(state == recordState.STOP){
+                    //Everything that happens at start here
+                    MillisecondTime = 0L;
+                    Seconds = 0;
+                    Minutes = 0;
+                    MilliSeconds = 0;
+                    duration.setText("00:00:00");
+                    //Option to Start
+                    recordButton.setText("Start");
+                    state = recordState.REST;
+                }
+                //Rest goes to start
+                else{
+                    StartTime = SystemClock.uptimeMillis();
+                    handler.postDelayed(runnable, 0);
+                    //Option to stop
+                    recordButton.setText("Stop");
+                    state = recordState.START;
+                }
+            }
+        });
+        duration = (TextView)findViewById(R.id.duration);
+        handler = new Handler();
     }
+
+    public Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            MillisecondTime = SystemClock.uptimeMillis() - StartTime;
+            UpdateTime = TimeBuff + MillisecondTime;
+            Seconds = (int)(UpdateTime/1000);
+            Minutes = Seconds / 60;
+            Seconds = Seconds % 60;
+            MilliSeconds = (int) (UpdateTime % 100);
+            duration.setText(String.format("" + String.format("%02d", Minutes) + ":" + String.format("%02d", Seconds)
+                    + ":" + String.format("%02d", MilliSeconds)));
+            handler.postDelayed(this, 0);
+        }
+    };
 
     private void getDeviceLocation() {
         mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
@@ -137,6 +181,29 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     initMap();
                 }
             }
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        Toast.makeText(this, "Map is Ready HERE", Toast.LENGTH_SHORT).show();
+        mMap = googleMap;
+        Log.d(TAG, "" + mLocationPermissionGranted);
+        if (mLocationPermissionGranted) {
+            getDeviceLocation();
+            //mark location
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            mMap.setMyLocationEnabled(true);
         }
     }
 }
