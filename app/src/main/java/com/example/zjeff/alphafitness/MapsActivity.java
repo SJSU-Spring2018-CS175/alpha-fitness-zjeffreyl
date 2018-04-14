5d4a60abb3d316610397b15aa263addc778b1779
zjeffreyl <zjeffreyl@gmail.com>
2018-04-13 19:11:15 -0700

    MapsActivity with camera follow on start and GPS

--- app/src/main/java/com/example/zjeff/alphafitness/MapsActivity.java
+++ app/src/main/java/com/example/zjeff/alphafitness/MapsActivity.java
@@ -3,7 +3,10 @@ package com.example.zjeff.alphafitness;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Criteria;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.nfc.Tag;
 import android.os.Handler;
 import android.os.SystemClock;
@@ -15,12 +18,14 @@ import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -30,7 +35,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback{
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener {
 
     private GoogleMap mMap;
     private static final String TAG = "MapActivity";
@@ -40,6 +45,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
     private FusedLocationProviderClient mFusedLocationProviderClient;
     private static final float DEFAULT_ZOOM = 15f;
+    private CompoundButton mAnimateToggle;
     Button recordButton;
     ImageButton profileButton;
     //Stopwatch
@@ -47,8 +53,36 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     int MilliSeconds, Seconds, Minutes;
     long MillisecondTime, StartTime, UpdateTime, TimeBuff = 0L;
     Handler handler;
+
+    LocationManager locationManager;
+
+    @Override
+    public void onLocationChanged(Location location) {
+        if(state == recordState.START) {
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(location.getLatitude(), location.getLongitude())));
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+
     //record states
-    public enum recordState{START, STOP, REST};
+    public enum recordState {
+        START, STOP, REST
+    }
+
+    ;
     public recordState state = recordState.REST;
 
     @Override
@@ -56,7 +90,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         getLocationPermission();
-        profileButton = (ImageButton)findViewById(R.id.profile);
+        profileButton = (ImageButton) findViewById(R.id.profile);
         profileButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -65,42 +99,43 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
         });
         //Stopwatch
-        recordButton = (Button)findViewById(R.id.recordButton);
+        recordButton = (Button) findViewById(R.id.recordButton);
         recordButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //Start goes to stop
-                if(state == recordState.START){
+                if (state == recordState.START) {
                     //Everything that happens at stop here
                     TimeBuff += MillisecondTime;
                     handler.removeCallbacks(runnable);
                     //Option to Reset
                     recordButton.setText("Reset");
                     state = recordState.STOP;
-                }else
-                //Stop goes to rest
-                if(state == recordState.STOP){
-                    //Everything that happens at start here
-                    MillisecondTime = 0L;
-                    Seconds = 0;
-                    Minutes = 0;
-                    MilliSeconds = 0;
-                    duration.setText("00:00:00");
-                    //Option to Start
-                    recordButton.setText("Start");
-                    state = recordState.REST;
-                }
-                //Rest goes to start
-                else{
-                    StartTime = SystemClock.uptimeMillis();
-                    handler.postDelayed(runnable, 0);
-                    //Option to stop
-                    recordButton.setText("Stop");
-                    state = recordState.START;
-                }
+                } else
+                    //Stop goes to rest
+                    if (state == recordState.STOP) {
+                        //Everything that happens at start here
+                        MillisecondTime = 0L;
+                        Seconds = 0;
+                        Minutes = 0;
+                        MilliSeconds = 0;
+                        duration.setText("00:00:00");
+                        //Option to Start
+                        recordButton.setText("Start");
+                        state = recordState.REST;
+                    }
+                    //Rest goes to start
+                    else {
+                        StartTime = SystemClock.uptimeMillis();
+                        handler.postDelayed(runnable, 0);
+                        //Option to stop
+                        recordButton.setText("Stop");
+                        //lerp camera lock
+                        state = recordState.START;
+                    }
             }
         });
-        duration = (TextView)findViewById(R.id.duration);
+        duration = (TextView) findViewById(R.id.duration);
         handler = new Handler();
     }
 
@@ -109,7 +144,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         public void run() {
             MillisecondTime = SystemClock.uptimeMillis() - StartTime;
             UpdateTime = TimeBuff + MillisecondTime;
-            Seconds = (int)(UpdateTime/1000);
+            Seconds = (int) (UpdateTime / 1000);
             Minutes = Seconds / 60;
             Seconds = Seconds % 60;
             MilliSeconds = (int) (UpdateTime % 100);
@@ -120,31 +155,33 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     };
 
     private void getDeviceLocation() {
-        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
-        try {
-            if (mLocationPermissionGranted) {
-                Task location = mFusedLocationProviderClient.getLastLocation();
-                location.addOnCompleteListener(new OnCompleteListener() {
-                    @Override
-                    public void onComplete(@NonNull Task task) {
-                        if (task.isSuccessful()) {
-                            Log.d(TAG, "onComplete: found location");
-                            Location currentLocation = (Location) task.getResult();
-                            moveCamera(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()), DEFAULT_ZOOM);
-                        } else {
-                            Log.d(TAG, "onComplete: current location is null");
-                            Toast.makeText(MapsActivity.this, "unable to provide current location", Toast.LENGTH_SHORT);
-                        }
-                    }
-                });
-            }
-        } catch (SecurityException e) {
-            Log.e(TAG, "getDeviceLocation: SecurityException: " + e.getMessage());
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        String provider = locationManager.getBestProvider(criteria, true);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
         }
+        Location location = locationManager.getLastKnownLocation(provider);
+        moveCamera(new LatLng(location.getLatitude(), location.getLongitude()), DEFAULT_ZOOM);
+        locationManager.requestLocationUpdates(provider, 1000, 0, this);
+    }
+
+    private void drawMarker(Location location){
+        mMap.clear();
+        LatLng currentPosition = new LatLng(location.getLatitude(), location.getLongitude());
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentPosition, DEFAULT_ZOOM));
+        mMap.addMarker(new MarkerOptions().position(currentPosition).snippet("Lat: " + location.getLatitude() + " Lng: " + location.getLongitude()));
     }
 
     private void moveCamera(LatLng latLng, float zoom) {
-        Log.d(TAG, "moveCamera: moving the camerae to: lat: " + latLng.latitude + ", lng: " + latLng.longitude);
+        Log.d(TAG, "moveCamera: moving the camera to: lat: " + latLng.latitude + ", lng: " + latLng.longitude);
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
     }
 
